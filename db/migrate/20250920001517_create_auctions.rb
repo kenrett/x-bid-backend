cb0deaa88e59337b32f50e7203500fb31e1497ac
Ken Rettberg <kenrettberg@gmail.com>
2025-09-19 17:17:46 -0700

    Adds auctions

---
+++ db/migrate/20250920001517_create_auctions.rb
@@ -0,0 +1,14 @@
+class CreateAuctions < ActiveRecord::Migration[8.0]
+  def change
+    create_table :auctions do |t|
+      t.string :title
+      t.text :description
+      t.datetime :start_date
+      t.decimal :current_price, precision: 4, scale: 2
+      t.string :image_url
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
