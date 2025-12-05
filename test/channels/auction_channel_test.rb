87f6043a25d4a9ed183373a835140407c807c9b6
Ken Rettberg <kenrettberg@gmail.com>
2025-12-04 16:44:53 -0800

    Adds AuctionChannel tests

---
+++ test/channels/auction_channel_test.rb
@@ -0,0 +1,52 @@
+require "test_helper"
+
+class AuctionChannelTest < ActionCable::Channel::TestCase
+  def setup
+    @auction = Auction.create!(
+      title: "Auction",
+      description: "Desc",
+      start_date: Time.current,
+      end_time: 1.hour.from_now,
+      current_price: 1.0,
+      status: :active
+    )
+  end
+
+  test "rejects subscription when auction_id is missing" do
+    subscribe
+    assert subscription.rejected?
+  end
+
+  test "rejects subscription when auction does not exist" do
+    subscribe(auction_id: 999_999)
+    assert subscription.rejected?
+  end
+
+  test "streams for valid auction" do
+    subscribe(auction_id: @auction.id)
+    assert subscription.confirmed?
+    assert_has_stream_for @auction
+  end
+
+  test "stop_stream stops stream" do
+    subscribe(auction_id: @auction.id)
+    perform :stop_stream
+    refute stream_exists_for?(@auction)
+  end
+
+  test "start_stream resumes stream" do
+    subscribe(auction_id: @auction.id)
+    perform :stop_stream
+    refute stream_exists_for?(@auction)
+    perform :start_stream
+    assert stream_exists_for?(@auction)
+  end
+
+  private
+
+  def stream_exists_for?(auction)
+    streams = subscription.send(:streams)
+    target = subscription.send(:broadcasting_for, auction)
+    streams&.include?(target)
+  end
+end
