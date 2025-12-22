626f332a2a87e4a70061aa60fe84aad89946378d
Ken Rettberg <kenrettberg@gmail.com>
2025-12-22 13:19:49 -0700

    Updates frontend origins in creds

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   # -----------------------------
   config.action_cable.url = ENV.fetch("ACTION_CABLE_URL", "wss://x-bid-backend.onrender.com/cable")
   config.action_cable.allowed_request_origins = [
-    Eails.application.credentials.frontend_origins,
+    Rails.application.credentials.frontend_origins,
     "https://x-bid-backend.onrender.com",
     ENV["CUSTOM_DOMAIN"]
   ].compact
