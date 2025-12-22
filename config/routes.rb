1a51fb893bba3ba779941d61e03832abb02c3747
Ken Rettberg <kenrettberg@gmail.com>
2025-12-22 13:45:40 -0700

    Mounts Action Cable endpoint and allows external scripts/multi-origin CORS

--- config/routes.rb
+++ config/routes.rb
@@ -68,6 +68,8 @@ Rails.application.routes.draw do
   # Quiet missing favicon to avoid log noise
   get "/favicon.ico", to: proc { [ 204, { "Content-Type" => "image/x-icon" }, [] ] }
 
+  mount ActionCable.server => "/cable"
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
